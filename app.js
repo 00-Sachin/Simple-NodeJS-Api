ffff4f671b43c86b0823a0b7a3dbc409148cc7f0
00-Sachin <thesachin121@gmail.com>
2026-07-08 16:00:11 +0530

    updated the code of app.js to make it runable

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ app.get('/status', (req, res) => {
   res.status(200).json({ status: 'API is running perfectly!' });
 });
 
+// Start the server on port 3000 (only if not running tests)
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
+}
+
 module.exports = app;
